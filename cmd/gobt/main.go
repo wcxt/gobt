d46d533c9aa3f67a5a0ed1664d28bea114f76fcd
edwces <68016750+edwces@users.noreply.github.com>
2023-11-06 19:21:57 +0100

    Handling bitfield and have messages

--- cmd/gobt/main.go
+++ cmd/gobt/main.go
@@ -11,7 +11,10 @@ import (
 	"github.com/edwces/gobt/message"
 )
 
-const RequestLength = 16000
+const (
+    RequestLength = 16000
+    MaxPipelinedRequests = 5
+)
 
 func main() {
 	path := os.Args[1]
@@ -42,112 +45,99 @@ func main() {
 		return
 	}
 
+    hashes, err := metainfo.Info.Hashes()
+    if err != nil {
+		fmt.Println(err)
+		return
+	}
     
-    missing := make(chan int, metainfo.Info.Length)
+    missing := make([]int, len(hashes))
     downloaded := make(chan []byte, metainfo.Info.Length)
     
     // Connect to peers
-    for _, peer := range peers {
-        go func(peer gobt.AnnouncePeer, in chan int, out chan []byte) {
-            // Establish conn
-            conn, err := net.DialTimeout("tcp", peer.Addr(), 3 * time.Second) 
+    go func(peer gobt.AnnouncePeer, in []int, out chan []byte) {
+        // Establish conn
+        conn, err := net.DialTimeout("tcp", peer.Addr(), 3 * time.Second) 
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+
+        defer conn.Close()
+        // Handshake
+        hs := handshake.New(hash, clientID)
+        handshake.Write(conn, hs)
+
+        hs, err = handshake.Read(conn)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+
+        if hs.InfoHash != hash {
+            return
+        }
+        
+        // Message loop
+        //choked := true
+        interesting := false
+        //choking := true
+        //interested := false
+
+        downloadable := []int{}
+        
+        for {
+            msg, err := message.Read(conn)
             if err != nil {
                 fmt.Println(err)
                 return
             }
 
-            defer conn.Close()
-            // Handshake
-            hs := handshake.New(hash, clientID)
-            handshake.Write(conn, hs)
-
-            hs, err = handshake.Read(conn)
-            if err != nil {
-                fmt.Println(err)
-                return
-            }
-
-            if hs.InfoHash != hash {
-                return
-            }
-            
-            // Message loop
-            //choked := true
-            interesting := false
-            //choking := true
-            //interested := false
-
-            downloadable := []int{}
-            //requests := []*message.Request{}
-            for {
-                msg, err := message.Read(conn)
-                if err != nil {
-                    fmt.Println(err)
-                    return
+            switch msg.ID {
+            //case message.IDChoke:
+            //case message.IDUnchoke:
+            //case message.IDInterested:
+            //case message.IDNotInterested:
+            //case message.IDPiece:
+            case message.IDHave:
+                have := int(msg.Payload.Have())
+                
+                for index := range missing {
+                    if have == index {
+                        downloadable = append(downloadable, index)
+                    }
                 }
-
-                switch msg.ID {
-                case message.IDChoke:
-                    //choking = true
-                case message.IDUnchoke:
-                    //choking = false
-                    // dump all pipelined request for piece
-                    //for _, request := range requests {
-                    //    msg := &message.Message{ID: message.IDRequest, Payload: message.NewRequestPayload(*request)} 
-                    //    _, err := message.Write(conn, msg)
-                    //    if err != nil {
-                    //        fmt.Println(err)
-                    //        return
-                    //    }
-                    //}
-
-                case message.IDInterested:
-                    //interested = true
-                case message.IDNotInterested:
-                    //interested = false
-                case message.IDHave:
-                    have := int(msg.Payload.Have())
-                    
-                    // TODO: Change how we handle which pieces we can download
-                    for index := range missing {
-                        if have == index {
-                            downloadable = append(downloadable, index)
-                        }
-                        missing<-index
+                
+                if len(downloadable) != 0 && !interesting {
+                    msg := &message.Message{ID: message.IDInterested}
+                    _, err := message.Write(conn, msg)
+                    if err != nil {
+                        fmt.Println(err)
+                        return
                     }
-
-                    if len(downloadable) != 0 && !interesting {
-                        msg := &message.Message{ID: message.IDInterested}
-                        _, err := message.Write(conn, msg)
-                        if err != nil {
-                            fmt.Println(err)
-                            return
-                        }
+                    interesting = true
+                }
+            case message.IDBitfield:
+                bitfield := msg.Payload.Bitfield()
+                
+                for index := range missing {
+                    if bitfield.Get(index) {
+                        downloadable = append(downloadable, index)
                     }
-                case message.IDBitfield:
-                    bitfield := msg.Payload.Bitfield()
-                    
-                    // TODO: Change how we handle which pieces we can download
-                    for index := range missing {
-                        if bitfield.Get(index) {
-                            downloadable = append(downloadable, index)
-                        }
-                        missing<-index
+                }
+                
+                if len(downloadable) != 0 && !interesting {
+                    msg := &message.Message{ID: message.IDInterested}
+                    _, err := message.Write(conn, msg)
+                    if err != nil {
+                        fmt.Println(err)
+                        return
                     }
-
-                    if len(downloadable) != 0 && !interesting {
-                        msg := &message.Message{ID: message.IDInterested}
-                        _, err := message.Write(conn, msg)
-                        if err != nil {
-                            fmt.Println(err)
-                            return
-                        }
-                    }
-                case message.IDPiece:
-                }    
-            }
-        }(peer, missing, downloaded)
-    }
+                    interesting = true
+                }
+            }    
+        }
+    }(peers[0], missing, downloaded)
 
     for block := range downloaded {
         fmt.Println(block)
