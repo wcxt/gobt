27658623e3b3922ecaf36e0f5adf59ad8cc39603
edwces <68016750+edwces@users.noreply.github.com>
2023-12-22 13:03:28 +0100

    Block payload tests

--- message/payload_test.go
+++ message/payload_test.go
@@ -95,4 +95,29 @@ func TestPayloadRequest(t *testing.T) {
     }
 }
 
+func TestNewBlockPayload(t *testing.T) {
+    block := message.Block{Index: 1467, Offset: 64000, Block: []byte{0x00, 0xE2, 0x06, 0xAB, 0x00, 0x31}}
 
+    got := message.NewBlockPayload(block)
+    want := message.Payload([]byte{0x00, 0x00, 0x05, 0xBB,
+                                   0x00, 0x00, 0xFA, 0x00,
+                                   0x00, 0xE2, 0x06, 0xAB, 0x00, 0x31})
+
+    if !bytes.Equal(got, want) {
+        t.Fatalf("got %#v, want %#v", got, want)
+    }
+}
+
+func TestPayloadBlock(t *testing.T) {
+    payload := message.Payload([]byte{0x00, 0x00, 0x05, 0xBB,
+                                   0x00, 0x00, 0xFA, 0x00,
+                                   0x00, 0xE2, 0x06, 0xAB, 0x00, 0x31})
+    
+    got := payload.Block()
+    want := message.Block{Index: 1467, Offset: 64000, Block: []byte{0x00, 0xE2, 0x06, 0xAB, 0x00, 0x31}}
+
+    if !reflect.DeepEqual(got, want) {
+        t.Fatalf("got %#v, want %#v", got, want)
+    }
+
+}
